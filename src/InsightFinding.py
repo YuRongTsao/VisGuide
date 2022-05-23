3ef2ea0445bf6198569d749f1e5364eb05e4ffd7
YuRongTsao <tammylinca@gmail.com>
2022-05-23 23:44:08 +0800

    code review

--- src/InsightFinding.py
+++ src/InsightFinding.py
@@ -1,15 +1,10 @@
-# Goal
 # Input : raw data
 # Function
 #   1.detect column type 
 #   2.enumerate combination
 #   3.insight detection functions
-import json
-from collections import defaultdict
-import statistics
+
 from scipy import stats
-from copy import deepcopy
-import time
 
 annotations = {
     'max' : '%s has the highest value %.2f for %s of %s ',
@@ -17,14 +12,15 @@ annotations = {
     'increase' : '+ %.2f'
 }
 
-#find insight
+# find features
 def findExtreme(vis):
     #找最大值 + z score
-    if len(vis.subgroup) ==0:
+    if len(vis["subgroup"]) ==0:
         return None
-    subgroup = vis.subgroup
-    keys = list(subgroup.keys())
-    values = list(subgroup.values())
+    
+    keys = list(vis["subgroup"].keys())
+    values = list(vis["subgroup"].values())
+    
     if len(values)>2:
         zScore = stats.zscore(values)
         pValues = list(map(lambda k : stats.norm.sf(abs(k))*2,zScore))
@@ -32,27 +28,18 @@ def findExtreme(vis):
         p_value = 1-((max(values) - min(values)) / sum(values)) if sum(values)!=0 else 1
         pValues = [p_value,p_value]
 
-    try:
-        maxValue = max(values)
-    except:
-        print('bad man')
-    i = values.index(maxValue)
-    maxKey = keys[i]
-    maxAnnotation = annotations['max'] % (maxKey,maxValue,vis.y_aggre,vis.y)
-    obj1 = {'tag':0,'insightType':'max','key':maxKey,'value':maxValue,'description' : maxAnnotation,'sig' : pValues[i]}
-
-    minValue = min(values)
-    i = values.index(minValue)
-    minKey = keys[i]
-    minAnnotation = annotations['min'] % (minKey,minValue,vis.y_aggre,vis.y)    
-    obj2 = {'tag':1,'insightType':'min','key':minKey,'value':minValue,'description' : minAnnotation,'sig' : pValues[i]}
-
+    pValues = dict(zip(keys,pValues))
+    
+    # find max value in a vis and record it's key name
+    obj1 = {'tag':0,'insightType':'max','key':vis["max_key"],'value':vis["subgroup"][vis["max_key"]],'sig' : pValues[vis["max_key"]]}
+    obj2 = {'tag':1,'insightType':'min','key':vis["min_key"],'value':vis["subgroup"][vis["min_key"]],'sig' : pValues[vis["min_key"]]}
+    
     return  [obj1,obj2]
 
 def findDifDistribution(curr_vis,pre_vis):
     #有不一樣的地方就算insight (標比例有增加的地方)
-    curr_subgroup = curr_vis.subgroup
-    pre_subgroup = pre_vis.subgroup
+    curr_subgroup = curr_vis["subgroup"]
+    pre_subgroup = pre_vis["subgroup"]
     insights = []
     isInInsight = False
 
@@ -68,13 +55,13 @@ def findDifDistribution(curr_vis,pre_vis):
                 value = round((curr_per-pre_per),4)
                 if value >= 0.05:
                     #check if a bar insight is exit
-                    for insight in curr_vis.insights:
+                    for insight in curr_vis["insights"]:
                         if key == insight['key']: 
                             isInInsight = True 
                             break
                     if not isInInsight:
                         p_value = 1-value        
-                        annotation = annotations['increase'] % (value*100) + '%' if curr_vis.globalAggre =='per' else annotations['increase'] % (value)
+                        annotation = annotations['increase'] % (value*100) + '%' if curr_vis["globalAggre"] =='per' else annotations['increase'] % (value)
                         insights.append({'tag':2,'insightType':'increase','key':key,'value':value,'description' : annotation,'sig' : p_value})
     
     return insights
